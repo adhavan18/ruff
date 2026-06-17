ea167d9e4e0e61a9ead174cc3f38634e60504576
Charlie Marsh <charlie.r.marsh@gmail.com>
2026-06-17 06:57:25 -0400

    [ty] Keep containment guards independent of equality details

--- crates/ty_python_semantic/src/types/narrow.rs
+++ crates/ty_python_semantic/src/types/narrow.rs
@@ -2244,6 +2244,7 @@ impl<'db> NarrowingConstraintsBuilder<'db, '_> {
 
     fn evaluate_expr_not_in(
         &self,
+        lhs_ty: Type<'db>,
         rhs_ty: Type<'db>,
         rhs_expr: Option<&ast::Expr>,
     ) -> Option<Type<'db>> {
@@ -2298,7 +2299,7 @@ impl<'db> NarrowingConstraintsBuilder<'db, '_> {
             }
             ast::CmpOp::Is => Some(rhs_ty),
             ast::CmpOp::In => self.evaluate_expr_in(lhs_ty, rhs_ty, rhs_expr),
-            ast::CmpOp::NotIn => self.evaluate_expr_not_in(rhs_ty, rhs_expr),
+            ast::CmpOp::NotIn => self.evaluate_expr_not_in(lhs_ty, rhs_ty, rhs_expr),
             _ => None,
         }
     }
