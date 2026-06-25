962f5ea98d4a6ba68e01109082c2e0e51d7a87c2
Micha Reiser <micha@reiser.io>
2026-06-25 14:59:23 +0000

    [ty] Support multiple analysis environments

--- crates/ty_python_semantic/src/types/tuple.rs
+++ crates/ty_python_semantic/src/types/tuple.rs
@@ -20,6 +20,7 @@ use std::cmp::Ordering;
 use std::hash::Hash;
 
 use itertools::{Either, EitherOrBoth, Itertools};
+use ruff_python_ast::PythonVersion;
 use smallvec::{SmallVec, smallvec_inline};
 
 use crate::subscript::{Nth, OutOfBoundsError, PyIndex, PySlice, StepSizeZeroError};
@@ -31,7 +32,7 @@ use crate::types::{
     ApplyTypeMappingVisitor, BoundTypeVarInstance, ErrorContext, FindLegacyTypeVarsVisitor,
     IntersectionType, Type, TypeContext, TypeMapping, UnionBuilder, UnionType,
 };
-use crate::{Db, FxOrderSet, Program};
+use crate::{Db, FxOrderSet};
 use ty_python_core::Truthiness;
 use ty_python_core::definition::Definition;
 
@@ -1519,8 +1520,10 @@ impl<'db> Tuple<Type<'db>> {
     }
 
     /// Return the `TupleSpec` for the singleton `sys.version_info`
-    pub(crate) fn version_info_spec(db: &'db dyn Db) -> TupleSpec<'db> {
-        let python_version = Program::get(db).python_version(db);
+    pub(crate) fn version_info_spec(
+        db: &'db dyn Db,
+        python_version: PythonVersion,
+    ) -> TupleSpec<'db> {
         let int_instance_ty = KnownClass::Int.to_instance(db);
 
         // TODO: just grab this type from typeshed (it's a `sys._ReleaseLevel` type alias there)
